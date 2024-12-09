381116f6d675ddc8089757c0e61b9231dbcb9a42
Abdulganiyu Sulyman Ayo <91748818+SulymanAyomi@users.noreply.github.com>
2024-12-09 08:53:01 +0100

    Update rpc.ts

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -2,5 +2,5 @@ import { hc } from "hono/client";
 
 import { AppType } from "@/app/api/[[...route]]/route";
 
-console.log("hi")
+console.log("hiii")
 export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!)
